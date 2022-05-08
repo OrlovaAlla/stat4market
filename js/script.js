79d2ff690d5934d0da9ac7f8a47d6b1d33110cc8
Alla <alla.nest.1@yandex.ru>
2022-05-08 20:55:41 +0300

    create

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let burger = document.querySelector('.mobile-menu');
     let nav = document.querySelector('.nav');
 
+    const mQuery = window.matchMedia("(max-width: 768px)");
     let abouth2 = document.querySelector('.about__h2');
     let title_def = abouth2.textContent;
 
@@ -10,14 +11,16 @@ document.addEventListener('DOMContentLoaded', function(){
         nav.classList.toggle("open");
       });
 
-    window.addEventListener('resize', function(){
-        if(this.window.matchMedia("(max-width: 1020px)").matches) {
-            console.log("меньше 1020");
+
+    // замена содержимого заголовка второй секции сайта  
+    function handleTabletChange(e) {
+        if(mQuery.matches) {            
             abouth2.innerHTML="Как работает модуль Аналитика по API для Wildberries?";
-        } if else (this.window.matchMedia("(min-width: 1021px)").matches) {
+        } else if (window.matchMedia("(min-width: 769px)").matches) {            
             abouth2.innerHTML= title_def;
         }
-        
-    });
-
+    }    
+   
+    mQuery.addListener(handleTabletChange);
+    handleTabletChange(mQuery);
 });
