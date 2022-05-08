1659f4b1cb7369c8542060557a3b83506e5c04e1
Alla <alla.nest.1@yandex.ru>
2022-05-08 18:23:24 +0300

    create

--- js/script.js
+++ js/script.js
@@ -3,9 +3,21 @@ document.addEventListener('DOMContentLoaded', function(){
     let burger = document.querySelector('.mobile-menu');
     let nav = document.querySelector('.nav');
 
+    let abouth2 = document.querySelector('.about__h2');
+    let title_def = abouth2.textContent;
+
      burger.addEventListener("click", function (){
-        console.log('клик');
         nav.classList.toggle("open");
       });
+
+    window.addEventListener('resize', function(){
+        if(this.window.matchMedia("(max-width: 1020px)").matches) {
+            console.log("меньше 1020");
+            abouth2.innerHTML="Как работает модуль Аналитика по API для Wildberries?";
+        } else {
+            abouth2.innerHTML= title_def;
+        }
+        
+    });
 
 });
